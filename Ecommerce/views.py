9c86bb4335f0e30284d474b9ece18968009e8992
sujan5655 <chaudharysujan5655@gmail.com>
2026-09-19 22:03:10 +0545

    first commit

---
+++ Ecommerce/views.py
@@ -0,0 +1,17 @@
+# from django.http import HttpResponse
+# def home(request):
+#   return HttpResponse("Homepage")
+
+
+from django.shortcuts import render
+
+from store.models import Product
+
+def home(request):
+  products=Product.objects.all().filter(is_available=True)
+  context={
+    'products':products
+  }
+  return render(request,'home.html',context)
+
+
---
+++ store/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from store.models import Product
+
+# Create your views here.
+def store(request):
+  products=Product.objects.all().filter(is_available=True)
+  context={
+    'products':products
+  }
+  return render(request,'store/store.html',context)
